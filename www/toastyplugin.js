bdaf9efcc80997e408b4d5ea29173b341818e28e
cplusplus11 <luisalberto.huasacca@gmail.com>
2019-08-27 15:27:57 -0500

    Update toastyplugin.js

--- www/toastyplugin.js
+++ www/toastyplugin.js
@@ -1,11 +1,12 @@
-// Empty constructor
+/ Empty constructor
 function ToastyPlugin() {}
 
 // The function that passes work along to native shells
 // Message is a string, duration may be 'long' or 'short'
-ToastyPlugin.prototype.show = function(message, successCallback, errorCallback) {
+ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
   var options = {};
   options.message = message;
+  options.duration = duration;
   cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
 }
 
